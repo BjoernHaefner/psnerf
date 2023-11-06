d080339918cd389a36d8d471128737616dd29983
haefner <bjoern.haefner@tum.de>
2023-11-06 14:54:57 +0100

    edit quick_start; add convert.py

---
+++ convert.py
@@ -0,0 +1,114 @@
+import logging
+import shutil
+import sys
+from os import listdir, sep
+from os.path import abspath, isdir, isfile, join
+
+import numpy as np
+from PIL.Image import Image
+from imageio import imread
+
+logFormatter = logging.Formatter(
+    "%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+rootLogger = logging.getLogger()
+rootLogger.setLevel(logging.INFO)
+
+convert_log_file = 'convert.log'
+fileHandler = logging.FileHandler(convert_log_file)
+fileHandler.setFormatter(logFormatter)
+rootLogger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler(sys.stdout)
+consoleHandler.setFormatter(logFormatter)
+rootLogger.addHandler(consoleHandler)
+
+
+class Convert2PSNeRF:
+    '''Converts our data to the data format of PSNeRF'''
+
+    def __init__(self, path2dataset: str):
+        self.path2dataset = path2dataset
+        self.datasets = {}
+        for name in sorted(listdir(self.path2dataset)):
+            if isdir(join(self.path2dataset, name)):
+                rootLogger.info(f'Found dataset: {abspath(join(self.path2dataset, name))}')
+                self.datasets[name] = {'path': self.path2dataset,
+                                       'name': name}
+                dataset = self.datasets[name]
+                data = listdir(join(dataset['path'], dataset['name']))
+                for d in sorted(data):
+                    if 'cameras' in d:
+                        dataset['cameras'] = d
+                    if 'image' in d:
+                        dataset['images'] = {'name': d}
+                    if 'directional_light' in d:
+                        dataset['directional_lights'] = d
+                    if 'mask' in d:
+                        dataset['masks'] = {'name': d}
+                rootLogger.info(dataset)
+        return
+
+    def __call__(self, output_path: str, dry: bool = True, force: bool = False):
+        for dataset in self.datasets.values():
+            rootLogger.info(f'Prepare {dataset["name"]}')
+            # images
+            for vi, view in enumerate(self._list_files(
+                    join(dataset['path'], dataset['name'], dataset['images']['name'])), 1):
+                imgs = []
+                for img in self._list_files(
+                        join(dataset['path'], dataset['name'], dataset['images']['name'], view)):
+                    img_name = join(dataset['path'], dataset['name'], dataset['images']['name'],
+                                    view, img)
+                    if 'ambient' in img:
+                        rootLogger.info(f"Read ambient: {img_name}")
+                        ambient_image = imread(img_name).astype(np.float32) / 255.0
+                    else:
+                        rootLogger.info(f"Read PS+ambient: {img_name}")
+                        imgs.append(imread(img_name).astype(np.float32) / 255.0)
+                rootLogger.info(f"Substract ambient image")
+                imgs = np.stack(imgs, axis=0) - ambient_image
+                for ii, img in enumerate(imgs, 1):
+                    # psnerf data structure
+                    img_out_path = join(output_path,
+                                        dataset['name'], 'img', f'view_{vi:02d}', f'{ii:03d}.png')
+                    if not isfile(img_out_path) or force:
+                        rootLogger.info(f'Write: {img_out_path}')
+                        if not dry:
+                            Image.fromarray(img).save(img_out_path)
+            # masks
+            for mi, mask in enumerate(self._list_files(
+                    join(dataset['path'], dataset['name'], dataset['masks']['name'])), 1):
+                mask_name = join(dataset['path'], dataset['name'], dataset['masks']['name'], mask)
+                mask_out_path = join(output_path, dataset['name'], 'mask', f'view_{mi:02d}.png')
+                if not isfile(mask_out_path) or force:
+                    rootLogger.info(f"cp {mask_name} {mask_out_path}")
+                    if not dry:
+                        shutil.copy2(mask_name, mask_out_path)
+
+                norm_mask_out_path = join(output_path, dataset['name'], 'norm_mask',
+                                          f'view_{mi:02d}.png')
+                if not isfile(norm_mask_out_path) or force:
+                    rootLogger.info(f"cp {mask_name} {norm_mask_out_path}")
+                    if not dry:
+                        shutil.copy2(mask_name, norm_mask_out_path)
+
+            # cameras
+
+            # light
+        return
+
+    def _list_files(self, path: str):
+        return sorted(listdir(abspath(path)))
+
+
+if __name__ == "__main__":
+    path2dataset = abspath(
+        join(sep, 'storage', 'group', 'cvpr', 'brahimi', 'multiview_PS_project', 'data',
+             'PS_final_evaluation_dataset', 'photometric'))
+    convert = Convert2PSNeRF(path2dataset)
+
+    output_path = abspath('dataset')
+    convert(output_path)
+
+# rootLogger.info(f"mv {convert_log_file} {join(output_path, 'convert.log')}")
+# shutil.move(convert_log_file, join(output_path, 'convert.log'))
--- quick_start.py
+++ quick_start.py
@@ -1,14 +1,30 @@
 import os
 import sys
 from subprocess import call
-from typing import List
+from typing import List, Tuple
 
+from pyhocon import ConfigFactory
 
-def SAVE_CALL(cmd: str, args: List[str], dry: bool = False):
-    print(f"Call: {' '.join([cmd, *args])}")
+import stage1.dataloading as dl
+
+'''
+This script aims to simplify the process of running all steps and stages of psnerf.
+It currently only works with the bear dataset, as some minor adaptions need to be made in the config file:
+stage1/configs/{OBJ_NAME}: trainig/out_dir: {EXPFOLDER}/{OBJ_NAME}/{EXPNAME}/ [This repo expects [hardcoded] the center folder name to be always {OBJ_NAME}, and to have exactly three directories]
+
+Troubleshooting:
+I had to follow this:
+    https://github.com/conda-forge/pytorch_sparse-feedstock/issues/21#issuecomment-1056418260
+as the original repos environment.yaml file threw the error mentioned in above issue when running stage1/shape_extract.py
+'''
+
+file_dir = os.path.dirname(os.path.realpath(sys.argv[0])) # directory where this file lies
+
+def SAVE_CALL(cmd: str, args: List[str], goto: str, dry: bool = False):
+    print(f"Call: {' '.join([f'cd {os.path.join(file_dir, goto)};', cmd, *args, ';', f'cd {file_dir}'])}")
     if not dry:
         try:
-            retcode = call(" ".join([cmd, *args]), shell=True)
+            retcode = call(" ".join([f'cd {os.path.join(file_dir, goto)};', cmd, *args, ";", f'cd {file_dir}']), shell=True)
             if retcode < 0:
                 print("Child was terminated by signal", -retcode, file=sys.stderr)
             else:
@@ -27,7 +43,7 @@ def preprocessing(obj_name: str, gpu_id: int):
         f"--benchmark UPS_Custom_Dataset",
         f"--bm_dir dataset/{obj_name}",
     ]
-    SAVE_CALL(cmd, args)
+    SAVE_CALL(cmd, args, 'preprocessing')
     return
 
 
@@ -39,113 +55,127 @@ def light_avg(obj_name: str):
         f"--light_intnorm",
         f"--sdps",
     ]
-    SAVE_CALL(cmd, args)
+    SAVE_CALL(cmd, args, ".")
     return
 
 
-def stage1(obj_name: str, expname: str, gpu_id: int, test: bool = False, gt_mesh: str = ""):
+def stage1(obj_name: str, gpu_id: int, calls: Tuple[str], gt_mesh: str = ""):
+    cfg = dl.load_config(f"stage1/configs/{obj_name}.yaml")
+    expfolder = cfg['training']['out_dir'].rstrip('/').split('/')[0] # "out"
+    expname = cfg['training']['out_dir'].rstrip('/').split('/')[-1] # "test_1"
     def train():
-        cmd = f"python stage1/train.py"
+        cmd = f"python train.py"
         args = [
-            f"stage1/configs/{obj_name}.yaml",
+            f"configs/{obj_name}.yaml",
             f"--gpu {gpu_id}",
         ]
-        SAVE_CALL(cmd, args)
+        SAVE_CALL(cmd, args, "stage1")
         return
 
     def shape_extract():
-        cmd = f"python stage1/shape_extract.py"
+        cmd = f"python shape_extract.py"
         args = [
             f"--gpu {gpu_id}",
+            f"--exp_folder {expfolder}",
             f"--obj_name {obj_name}",
             f"--expname {expname}",
             f"--visibility",
             f"--vis_plus",
         ]
-        SAVE_CALL(cmd, args)
+        SAVE_CALL(cmd, args, "stage1")
         return
 
-    train()
-    shape_extract()
+    def eval():
+        cmd = f"python eval.py"
+        args = [
+            f"--gpu {gpu_id}",
+            f"--exp_folder {expfolder}",
+            f"--obj_name {obj_name}",
+            f"--expname {expname}",
+        ]
+        SAVE_CALL(cmd, args, "stage1")
+        return
 
-    if test:
+    def extract_mesh():
         '''
-        Run before: 
+        Run before:
         $ python setup.py build_ext --inplace
         '''
+        cmd = f"python extract_mesh.py"
+        args = [
+            f"--gpu {gpu_id}",
+            f"--exp_folder {expfolder}",
+            f"--obj_name {obj_name}",
+            f"--expname {expname}",
+        ]
+        SAVE_CALL(cmd, args, "stage1")
+        return
 
-        def eval():
-            cmd = f"python stage1/eval.py"
-            args = [
-                f"--gpu {gpu_id}",
-                f"--obj_name {obj_name}",
-                f"--expname {expname}",
-            ]
-            SAVE_CALL(cmd, args)
-            return
+    def chamfer_dist():
+        cmd = f"python ../chamfer_dist.py"
+        args = [
+            f"--mesh_gt {gt_mesh}",
+            f"--mesh_pred PRED_MESH_PATH", #todo: probably from previous routine extract_mesh
+        ]
+        SAVE_CALL(cmd, args, "stage1")
+        return
 
-        def extract_mesh():
-            cmd = f"python stage1/extract_mesh.py"
-            args = [
-                f"--gpu {gpu_id}",
-                f"--obj_name {obj_name}",
-                f"--expname {expname}",
-            ]
-            SAVE_CALL(cmd, args)
-            return
+    if "train" in calls:
+        train()
+    if "shape_extract" in calls:
+        shape_extract()
 
-        def chamfer_dist():
-            cmd = f"python chamfer_dist.py"
-            args = [
-                f"--mesh_gt {gt_mesh}",
-                f"--mesh_pred PRED_MESH_PATH",
-            ]
-            SAVE_CALL(cmd, args)
-            return
-
-        eval()
+    if "eval" in calls:
+        eval() # todo: test
+    if "extract_mesh" in calls:
         extract_mesh()
-        if os.path.isfile(gt_mesh):
-            chamfer_dist()
+    if "chamfer_dist" in calls and os.path.isfile(gt_mesh):
+        chamfer_dist() #todo: test
     return
 
 
-def stage2(obj_name: str, expname: str, gpu_id: int):
+def stage2(obj_name: str, gpu_id: int, calls: Tuple[str]):
+    conf = ConfigFactory.parse_file(f"stage2/confs/{obj_name}.conf")
+    expname = conf.get_string('train.expname')
     def train():
-        cmd = f"python stage2/train.py"
+        cmd = f"python train.py"
         args = [
-            f"--conf stage2/confs/{obj_name}.conf",
+            f"--conf confs/{obj_name}.conf",
             f"--gpu {gpu_id}",
+            f"--is_continue",
         ]
-        SAVE_CALL(cmd, args)
+        SAVE_CALL(cmd, args, "stage2")
         return
 
     def eval_envmap():
         # For rendering with environment lighting
-        cmd = f"python stage2/eval.py"
+        cmd = f"python eval.py"
         args = [
             f"--gpu {gpu_id}",
             f"--obj_name {obj_name}",
             f"--expname {expname}",
             f"--render_envmap",
         ]
-        SAVE_CALL(cmd, args)
+        SAVE_CALL(cmd, args, "stage2")
         return
 
-    def eval_material():
+    def eval():
         # For material editing
-        cmd = f"python stage2/eval.py"
+        cmd = f"python eval.py"
         args = [
             f"--gpu {gpu_id}",
             f"--obj_name {obj_name}",
             f"--expname {expname}",
         ]
-        SAVE_CALL(cmd, args)
+        SAVE_CALL(cmd, args, "stage2")
         return
 
-    train()
-    eval_envmap()
-    eval_material()
+    if "train" in calls:
+        train()
+    if "eval_envmap" in calls:
+        eval_envmap()
+    if "eval" in calls:
+        eval()
     return
 
 
@@ -156,7 +186,7 @@ def evaluation(obj_name: str, expname: str, test_out_dir: str):
         f"--expname {expname}",
         f"--test_out_dir {test_out_dir}",
     ]
-    SAVE_CALL(cmd, args)
+    SAVE_CALL(cmd, args, ".")
     return
 
 
@@ -166,20 +196,18 @@ def parser():
                                      description='Executes the whole PS-NeRF pipeline')
     parser.add_argument('-n', '--obj_name', type=str, help='object name')
     parser.add_argument('-g', '--gpu_id', type=int, default=0, help='GPU ID')
-    parser.add_argument('-e', '--expname', type=str, help='Experiment name')
     parser.add_argument('-o', '--test_out_dir', type=str, help='Output directory')
     return parser.parse_args(['-n', 'bear',
-                              '-e', 'initial_test_bear',
                               '-o', 'initial_test_bear_out',
                               ])
 
 
 def main(cli):
-    preprocessing(cli.obj_name)
-    light_avg(cli.obj_name)
-    stage1(cli.obj_name, cli.expname, cli.gpu_id)
-    stage2(cli.obj_name, cli.expname, cli.gpu_id)
-    evaluation(cli.obj_name, cli.expname, cli.test_out_dir)
+    # preprocessing(cli.obj_name, cli.gpu_id)
+    # light_avg(cli.obj_name)
+    # stage1(cli.obj_name, cli.gpu_id, calls=("extract_mesh"))
+    stage2(cli.obj_name, cli.gpu_id, calls=("eval"))
+    # evaluation(cli.obj_name, cli.test_out_dir)
     return
 
 
